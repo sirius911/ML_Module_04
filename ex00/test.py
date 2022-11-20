c2759f250ac171931e2ea24d4f1693c40d394cbc
Cyrille Lorin <cyrille.lorin@yahoo.fr>
2022-11-20 22:17:52 +0100

    ADD: ex00

---
+++ ex00/test.py
@@ -0,0 +1,12 @@
+import numpy as np
+from polynomial_model_extended import add_polynomial_features
+x = np.arange(1,11).reshape(5, 2)
+print(x)
+print(add_polynomial_features(x, 0))
+print(add_polynomial_features(x, 1))
+
+# Example 1:
+print(add_polynomial_features(x, 3))
+
+# Example 2:
+print(add_polynomial_features(x, 4))
