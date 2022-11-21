38379ed9d23239b379e8b7dec70f8eada08a8afe
Cyrille Lorin <cyrille.lorin@yahoo.fr>
2022-11-21 15:20:32 +0100

    ADD:ex02

---
+++ ex02/linear_loss_reg.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def reg_loss_(y, y_hat, theta, lambda_):
+    """Computes the regularized loss of a linear regression model from two non-empty numpy.array, without any for loop.
+    Args:
+        y: has to be an numpy.ndarray, a vector of shape m * 1.
+        y_hat: has to be an numpy.ndarray, a vector of shape m * 1.
+        theta: has to be a numpy.ndarray, a vector of shape n * 1.
+        lambda_: has to be a float.
+    Returns:
+        The regularized loss as a float.
+        None if y, y_hat, or theta are empty numpy.ndarray.
+        None if y and y_hat do not share the same shapes.
+    Raises:
+        This function should not raise any Exception.
+    """
+
+    m = y.shape[0]
+    t_ = np.squeeze(theta[1:])
+    loss = (y - y_hat).T @ (y - y_hat)
+    reg = lambda_ * t_ @ t_
+    return float(0.5 * (loss + reg) / m)
